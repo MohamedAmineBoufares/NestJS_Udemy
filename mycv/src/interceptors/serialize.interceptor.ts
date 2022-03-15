3b7b30df5dda7f58d90909824ee8ecbabb67cc94
Mohamed Amine <medamineboufares@gmail.com>
2022-03-15 16:59:21 +0100

    added serialization

---
+++ mycv/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Run smth before a req is handled by the req handler
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Run smth before the res is sent out
+
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -13,7 +13,12 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
+import { Serialize } from '../interceptors/serialize.interceptor';
+
+import { UserDto } from './dtos/user.dto';
+
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -23,6 +28,7 @@ export class UsersController {
   }
 
   // Nest parses params to Strings, even though it's a number in DB
+
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
