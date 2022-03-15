f88d8a6647cb7a79c422860e54cf23af0fadd996
Mohamed Amine <medamineboufares@gmail.com>
2022-03-15 14:06:25 +0100

    added CRUD operations

--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    // This creates an instance of an entity
+    const user = this.repo.create({ email, password });
+
+    // This will send that instance to DB
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    this.repo.remove(user);
+  }
+}
